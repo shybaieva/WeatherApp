36246e3c612903dca2180eb9a8b2f8b82ad4bef2
shybaieva <shybayeva13@gmail.com>
2020-07-08 20:42:14 +0300

    Weather added

--- app/src/main/java/net/shybaieva/weatherapp/MainActivity.java
+++ app/src/main/java/net/shybaieva/weatherapp/MainActivity.java
@@ -150,7 +150,6 @@ public class MainActivity extends AppCompatActivity   {
         protected void onPostExecute(String s) {
             try{
                 JSONObject jsonObject = new JSONObject (result);
-                //TODO weather and weather ico
 
 
                 Double temp = jsonObject.getJSONObject("main").getDouble("temp");
